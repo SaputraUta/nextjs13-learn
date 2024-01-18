b2cf61996eb7fa50c770b2b0b53b2553d5d57e41
Saputra <saputra.uta50@gmail.com>
2024-01-18 12:26:03 +0800

    Dynamic segments

---
+++ app/tickets/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TicketList from './TicketList'
+
+export default function page() {
+  return (
+    <main>
+        <nav>
+            <div>
+                <h2>Tickets</h2>
+                <p><small>Currently open tickets.</small></p>
+            </div>
+        </nav>
+        <TicketList/>
+    </main>
+  )
+}
