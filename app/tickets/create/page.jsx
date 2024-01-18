0372e041c0e261ca2acf3d687a0c78fb6a21c4fb
Saputra <saputra.uta50@gmail.com>
2024-01-18 14:00:37 +0800

    Add tickets

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm";
+
+export default function CreatTicket() {
+  return (
+    <main>
+      <h1 className="text-blue-700 font-bold text-center">Add a New Ticket</h1>
+      <CreateForm />
+    </main>
+  );
+}
