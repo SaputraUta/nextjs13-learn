017f8caa6c98069218f45caf5b7c6fba7ee84d28
Saputra <saputra.uta50@gmail.com>
2024-01-18 13:10:58 +0800

    Custom 404 page

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,22 @@
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
 async function getTicketDetail(id) {
   const res = await fetch("http://localhost:4000/tickets/" + id, {
     next: {
       revalidate: 60,
     },
   });
+  if (!res.ok) {
+    notFound();
+  }
   return res.json();
 }
 
